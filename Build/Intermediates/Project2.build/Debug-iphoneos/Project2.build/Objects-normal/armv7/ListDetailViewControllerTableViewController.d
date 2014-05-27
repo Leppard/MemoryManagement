afb2d11f94dfb99b9dee804c4a8cff6d3332fa7e
Li Zhenliang <coldlefthand@gmail.com>
2014-05-27 23:12:16 +0800

    C16

---
+++ Build/Intermediates/Project2.build/Debug-iphoneos/Project2.build/Objects-normal/armv7/ListDetailViewControllerTableViewController.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/lizhenliang/Desktop/ios/Project2/Project2/ListDetailViewControllerTableViewController.m \
+  /Users/lizhenliang/Desktop/ios/Project2/Project2/ListDetailViewControllerTableViewController.h \
+  /Users/lizhenliang/Desktop/ios/Project2/Project2/Checklist.h
